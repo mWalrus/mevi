ae6f13d8d8e3def9c23555ee80aed090aa9253af
Oskar Hellkvist <oskar.hellkvist@rapidimages.se>
2023-02-01 13:07:00 +0100

    feat(draft): begin implementation of shared memory stuff

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use x11rb::connection::Connection;
 use x11rb::image::ColorComponent;
 use x11rb::image::PixelLayout;
+use x11rb::protocol::shm::{self, ConnectionExt as _};
 use x11rb::protocol::xproto::{self, *};
 use x11rb::protocol::Event;
 
@@ -27,6 +28,13 @@ x11rb::atom_manager! {
 fn main() -> Result<()> {
     let (conn, screen_num) = x11rb::connect(None)?;
     let conn = &conn;
+
+    if let Some((major, minor)) = check_shm_version(conn)? {
+        if major < 1 || (major == 1 && minor < 2) {
+            eprintln!("X11 server supports version {major}.{minor} of the SHM extension, but version 1.2 is needed.");
+            return Ok(());
+        }
+    }
 
     let screen = &conn.setup().roots[screen_num];
 
@@ -89,10 +97,20 @@ fn main() -> Result<()> {
                 }
             }
             Event::Error(e) => eprintln!("Received error: {e:?}"),
-            ev => println!("Got an unknown event: {ev:?}"),
+            _ => {} // ev => println!("Got an unknown event: {ev:?}"),
         }
-        conn.flush()?;
     }
 
     Ok(())
+}
+
+pub fn check_shm_version<C: Connection>(conn: &C) -> Result<Option<(u16, u16)>> {
+    if conn
+        .extension_information(shm::X11_EXTENSION_NAME)?
+        .is_none()
+    {
+        return Ok(None);
+    }
+    let shm_version = conn.shm_query_version()?.reply()?;
+    Ok(Some((shm_version.major_version, shm_version.minor_version)))
 }

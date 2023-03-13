1f62d709756eb20f1f16a0d489e3339118f7a27e
mWalrus <hellkvistoskar@protonmail.com>
2023-03-13 13:14:52 +0100

    ref: move `RenderString` related items into separate file

--- src/font/mod.rs
+++ src/font/mod.rs
@@ -1,10 +1,12 @@
 pub mod loader;
+pub mod render_string;
 
-use self::loader::{FontEncodedChunk, LoadedFont};
+use self::loader::LoadedFont;
 use crate::{state::MeviState, util::Rect};
 use anyhow::Result;
+pub use render_string::{RenderLine, RenderString, ToRenderLine};
 use x11rb::{
-    protocol::render::{Color, ConnectionExt, Glyphset, PictOp},
+    protocol::render::{ConnectionExt, Glyphset, PictOp},
     rust_connection::RustConnection,
 };
 
@@ -111,59 +113,5 @@ impl FontDrawer {
         )?;
 
         Ok(())
-    }
-}
-
-pub struct RenderLine {
-    chunks: Vec<FontEncodedChunk>,
-    width: i16,
-    height: u16,
-}
-
-impl RenderLine {
-    pub fn new(drawer: &FontDrawer, text: impl ToString) -> Self {
-        let text = text.to_string();
-        let (width, height) = drawer.geometry(&text);
-        let chunks = drawer.font.encode(&text, width - 1);
-        Self {
-            chunks,
-            width,
-            height,
-        }
-    }
-
-    pub fn line_height(&self) -> u16 {
-        self.height
-    }
-}
-
-pub trait ToRenderLine {
-    fn to_lines(&self, font_drawer: &FontDrawer) -> Vec<RenderLine>;
-}
-
-pub struct RenderString {
-    pub lines: Vec<RenderLine>,
-    pub total_width: i16,
-    pub total_height: u16,
-    pub bg: Color,
-    pub fg: Color,
-}
-
-impl RenderString {
-    pub fn new(lines: Vec<RenderLine>, bg: Color, fg: Color) -> Self {
-        let mut total_width = 0;
-        for line in lines.iter() {
-            if line.width > total_width {
-                total_width = line.width;
-            }
-        }
-        let total_height = lines[0].line_height() * lines.len() as u16;
-        Self {
-            lines,
-            total_width,
-            total_height,
-            bg,
-            fg,
-        }
     }
 }
